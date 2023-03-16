8b6e9c45f4642bf92216d8317a6d14bda4cd6085
El Jefe <Tagne13@gmail.com>
2023-03-16 16:00:24 -0400

    added comments to js

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,47 @@
 $(document).ready(function () {
-// Search Variables
-const searchBtn = $('.searchBtn');
-const clearBtn = $('#clear-history');
-let searchCity = $('#search-city');
-let searchHistory = $('#search-history');
-var city = "";
+    // Search Variables
+    const searchBtn = $('.searchBtn');
+    const clearBtn = $('#clear-history');
+    let searchCity = $('#search-city');
+    let searchHistory = $('#search-history');
+    var city = "";
 
-// Current Variables
-const cityHeader = $('#city-date');
-const cityIcon = $('#city-icon');
-const cityTemp = $('#city-temp');
-const cityWind = $('#city-wind');
-const cityHumidity = $('#city-humidity');
+    // Current Variables
+    const cityHeader = $('#city-date');
+    const cityIcon = $('#city-icon');
+    const cityTemp = $('#city-temp');
+    const cityWind = $('#city-wind');
+    const cityHumidity = $('#city-humidity');
 
-// Date
-const todayDate = dayjs();
+    // Date
+    const todayDate = dayjs();
 
 
-var APIKey = "c684f4a2d3c668932b65951d98472409";
-var currentURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
-var futureURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+    var APIKey = "c684f4a2d3c668932b65951d98472409";
+    var currentURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+    var futureURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+
+    // Functions
+
+        // Current Weather 
+
+        // Generate content based on API response
+
+        // 5 Day Forecast
+
+        // Store search history
+
+        // Add searched cities to searched display
+
+    // Event Listeners
+
+    // Clear past searches
+    clearBtn.on("click", function () {
+        localStorage.clear();
+        searchHistory.empty();
+        location.reloard();
+    });
+
+    // Load default city or last search item
+
 });
