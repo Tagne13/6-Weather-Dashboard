e1e3f5233ee146b8aee0e26cd0be2c183379f8d7
El Jefe <Tagne13@gmail.com>
2023-03-16 13:09:45 -0400

    added variables to js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,23 @@
+$(document).ready(function () {
+// Search Variables
+const searchBtn = $('.searchBtn');
+const clearBtn = $('#clear-history');
+let searchCity = $('#search-city');
+let searchHistory = $('#search-history');
+var city = "";
+
+// Current Variables
+const cityHeader = $('#city-date');
+const cityIcon = $('#city-icon');
+const cityTemp = $('#city-temp');
+const cityWind = $('#city-wind');
+const cityHumidity = $('#city-humidity');
+
+// Date
+const todayDate = dayjs();
+
+
 var APIKey = "c684f4a2d3c668932b65951d98472409";
-var city;
 var currentURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
 var futureURL = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
-
+});
